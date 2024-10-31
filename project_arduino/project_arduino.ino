cfa35d89af35cf0d34e24a736fa1a86b2f447571
emchoi947 <emchoi@umich.edu>
2024-10-31 00:36:29 -0400

    code to read both TOF and thermal

--- project_arduino/project_arduino.ino
+++ project_arduino/project_arduino.ino
@@ -1,3 +1,4 @@
+#include "Adafruit_VL53L0X.h"
 #include <Wire.h>
 #include <Adafruit_AMG88xx.h>
 
@@ -5,40 +6,64 @@ Adafruit_AMG88xx amg;
 
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("AMG88xx pixels"));
+  Serial.begin(9600);
 
-    bool status;
+  bool status;
+  
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  //Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  status = amg.begin();
+  if (!status) {
+      Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+      while (1);
+  }
     
-    // default settings
-    status = amg.begin();
-    if (!status) {
-        Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-        while (1);
-    }
-    
-    Serial.println("-- Pixels Test --");
-
-    Serial.println();
-
-    delay(100); // let sensor boot up
+  // power 
+  //Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  delay(100);
 }
 
 
-void loop() { 
-    //read all the pixels
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    //Serial.print("Distance (mm): ");
+    Serial.print(measure.RangeMilliMeter);
+
+  } else {
+    Serial.print(-1);
+  }
+  Serial.print(",");
+
+    
+  //read all the pixels
     amg.readPixels(pixels);
 
-    // Send pixel data in a format easy to parse
+    //Serial.print("[");
     for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
       Serial.print(pixels[i]);
       if (i < AMG88xx_PIXEL_ARRAY_SIZE - 1) {
         Serial.print(",");
       }
     }
+    //Serial.println("]");
     Serial.println();
 
     //delay a second
-    delay(100);
+    delay(25);
 }
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import serial
+import numpy as np
 ser = serial.Serial('COM3', 9600, timeout = 1)  # open serial port
 print(ser.name)         # check which port was really used
 while True:
     try:
-        print(ser.readline().decode('utf-8').rstrip())
+        #print('start:')
+        data = ser.readline().decode('utf-8').rstrip()
+        datalist = data.split(",")
+        TOF1 = float(datalist[0])
+        thermal = [float(x) for x in datalist[1:]]
+        print(TOF1)
+        matrix = np.array(thermal).reshape(8,8)
+        print(matrix)
     except UnicodeDecodeError: # catch error and ignore it
         print('uh oh')
 
