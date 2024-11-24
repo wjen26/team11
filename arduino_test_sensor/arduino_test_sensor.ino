03272c864fbce7fdebe46bf012510727a91b9289
emchoi947 <emchoi@umich.edu>
2024-11-24 18:10:46 -0500

    trying to do zoning for infrared
    doesn't work ):

--- arduino_test_sensor/arduino_test_sensor.ino
+++ arduino_test_sensor/arduino_test_sensor.ino
@@ -4,12 +4,11 @@
 
 Adafruit_AMG88xx amg;
 
-float Tamb1 = 0; // Ambient temperature (in Celsius)
-float Tamb2 = 0;
+float Tamb= 0;
 float TH1_1 = 0;
 float TH1_2 = 0;
 float TH2 = 30.52;
-int binary_TH = 17;
+int binary_TH = 14;
 
 int cal_count = 1;
 
@@ -20,8 +19,8 @@ const float epsilon = 0.98;  // Emissivity (adjustable)
 int blob_count_top = 0;
 int blob_count_bottom = 0;
 
-float means1[10];
-float means2[10];
+float max_values1[10];
+float max_values2[10];
 
 void setup() {
   Serial.begin(9600);
@@ -40,12 +39,7 @@ void loop() {
 
   // Read pixels from sensor
   amg.readPixels(pixels);
-
-  float topPixels[32];
-  float bottomPixels[32];
-
-  memcpy(topPixels, pixels, sizeof(float) * 32);
-  memcpy(bottomPixels, &pixels[32], sizeof(float) * 32);
+  
 
   // // Print out the temperature data in an 8x8 grid format
   // for (int i = 0; i < 64; i++) {
@@ -56,58 +50,75 @@ void loop() {
   //   }
   // }
 
+  float topPixels[32];
+  float bottomPixels[32];
+
+  
+
   // 1.) calculate mean of array, set it to Tamb (ambient temperature)
   if (cal_count == 1) {
     Serial.print("Calibration step: ");
     Serial.println(cal_count);
-    // Tamb = calculateMean(pixels, 64);
-    // Serial.print("Tamb = ");
-    // Serial.println(Tamb);
-    Tamb1 = calculateMean(topPixels, 32);
-    Serial.print("Tamb1 = ");
-    Serial.println(Tamb1);
-    Tamb2 = calculateMean(bottomPixels, 32);
-    Serial.print("Tamb2 = ");
-    Serial.println(Tamb2);
+    Tamb = calculateMean(pixels, 64);
+    Serial.print("Tamb = ");
+    Serial.println(Tamb);
+    // Tamb1 = calculateMean(topPixels, 32);
+    // Serial.print("Tamb1 = ");
+    // Serial.println(Tamb1);
+    // Tamb2 = calculateMean(bottomPixels, 32);
+    // Serial.print("Tamb2 = ");
+    // Serial.println(Tamb2);
   }
 
   // 2-11.) change matrices to real temperatures based on formula
   if (cal_count > 1 && cal_count < 12) {
     Serial.print("Calibration step: ");
     Serial.println(cal_count);
+    for (int i = 0; i < 64; i++) {
+      pixels[i] = applyEquation(pixels[i]);
+      // topPixels[i] = applyEquation(topPixels[i], Tamb1);
+      // bottomPixels[i] = applyEquation(bottomPixels[i], Tamb2);
+    }
     for (int i = 0; i < 32; i++) {
-      // pixels[i] = applyEquation(pixels[i]);
-      topPixels[i] = applyEquation(topPixels[i], Tamb1);
-      bottomPixels[i] = applyEquation(bottomPixels[i], Tamb2);
+      topPixels[i] = pixels[i];  // Top half (first 32 pixels)
+      bottomPixels[i] = pixels[i + 32];  // Bottom half (next 32 pixels)
     }
     // 12.) calculate TH1 by getting the means of the 2-11 matrices and finding the mean and standard devs of the means, and TH1 is 2.5 standard devs above mean
-    // means[cal_count-2] = calculateMean(pixels, 64);
-    // Serial.print("Mean of current matrix: ");
-    // Serial.println(means[cal_count-2]);
-    means1[cal_count-2] = calculateMean(topPixels, 32);
-    Serial.print("Mean of current matrix 1: ");
-    Serial.println(means1[cal_count-2]);
-    means2[cal_count-2] = calculateMean(bottomPixels, 32);
-    Serial.print("Mean of current matrix 2: ");
-    Serial.println(means2[cal_count-2]);
+    // max_values[cal_count-2] = calculateMax(pixels, 64);
+    // Serial.print("Max value of current matrix: ");
+    // Serial.println(max_values[cal_count-2]);
+    max_values1[cal_count-2] = calculateMax(topPixels, 32);
+    Serial.print("Max value of current top matrix: ");
+    Serial.println(max_values1[cal_count-2]);
+    max_values2[cal_count-2] = calculateMax(bottomPixels, 32);
+    Serial.print("Max value of current bottom matrix: ");
+    Serial.println(max_values2[cal_count-2]);
     if (cal_count == 11) {
-      // float m = calculateMean(means, 10);
-      // TH1 = m + 3 * calculateStandardDeviation(means, 10, m);
+      // float m = calculateMean(max_values, 10);
+      // TH1 = m + 2.5 * calculateStandardDeviation(max_values, 10, m);
       // Serial.print("Threshold 1 = ");
       // Serial.println(TH1);
-      float m1 = calculateMean(means1, 10);
-      TH1_1 = m1 + 3 * calculateStandardDeviation(means1, 10, m1);
+      float m1 = calculateMean(max_values1, 10);
+      TH1_1 = m1 + 2.5 * calculateStandardDeviation(max_values1, 10, m1);
       Serial.print("Threshold 1 for top half = ");
       Serial.println(TH1_1);
-      float m2 = calculateMean(means2, 10);
-      TH1_2 = m2 + 3 * calculateStandardDeviation(means2, 10, m2);
+      float m2 = calculateMean(max_values2, 10);
+      TH1_2 = m2 + 2.5 * calculateStandardDeviation(max_values2, 10, m2);
       Serial.print("Threshold 1 for bottom half = ");
       Serial.println(TH1_2);
     }
   }
 
+  // actual detection logic
   if (cal_count < 12) { cal_count++; }
   else {
+    // memcpy(topPixels, pixels, sizeof(float) * 32);
+    // memcpy(bottomPixels, &pixels[32], sizeof(float) * 32);
+    for (int i = 0; i < 32; i++) {
+      topPixels[i] = pixels[i];  // Top half (first 32 pixels)
+      bottomPixels[i] = pixels[i + 32];  // Bottom half (next 32 pixels)
+    }
+
     // int TH2_count = 0;
     // for (int i = 0; i < 64; i++) {
     //   pixels[i] = applyEquation(pixels[i]);
@@ -127,49 +138,87 @@ void loop() {
     // }
     // Serial.print("Blob count: ");
     // Serial.println(blob_count);
-    int TH2_count_1 = 0;
+
+
+    int TH1_count_top = 0;
     for (int i = 0; i < 32; i++) {
-      topPixels[i] = applyEquation(topPixels[i], Tamb1);
-      if (topPixels[i] > TH2) {
-        TH2_count_1++;
+      topPixels[i] = applyEquation(topPixels[i]);
+      if (topPixels[i] > TH1_1) {
+        TH1_count_top++;
       }
     }
-    float avgValue1 = calculateMean(topPixels, 32);
-    if (avgValue1 < TH1_1) {
+    // float avgValue1 = calculateMean(topPixels, 32);
+    // if (avgValue1 < TH1_1) {
+    //   blob_count_top = 0;
+    // }
+    // else if (TH2_count_top > binary_TH) {
+    //   blob_count_top = 2;
+    // }
+    // else {
+    //   blob_count_top = 1;
+    if (TH1_count_top < 12) {
       blob_count_top = 0;
     }
-    else if (TH2_count_1 > binary_TH) {
-      blob_count_top = 2;
-    }
+    // else if (TH2_count_top > binary_TH) {
+    //   blob_count_top = 2;
+    // }
     else {
       blob_count_top = 1;
+
+      int maxIndex;
+      int maxValue = findMaxAndIndex(topPixels, 32, maxIndex);
+      Serial.print("Top max value at [");
+      Serial.print(maxIndex / 8);
+      Serial.print("][");
+      Serial.print(maxIndex % 8);
+      Serial.print("] = ");
+      Serial.println(maxValue);
     }
     Serial.print("Blob count for top half: ");
     Serial.println(blob_count_top);
     
-    int TH2_count_2 = 0;
+
+
+    int TH1_count_bottom = 0;
     for (int i = 0; i < 32; i++) {
-      bottomPixels[i] = applyEquation(bottomPixels[i], Tamb2);
-      if (bottomPixels[i] > TH2) {
-        TH2_count_2++;
+      bottomPixels[i] = applyEquation(bottomPixels[i]);
+      if (bottomPixels[i] > TH1_2) {
+        TH1_count_bottom++;
       }
     }
-    float avgValue2 = calculateMean(bottomPixels, 32);
-    if (avgValue2 < TH1_2) {
+    // float avgValue2 = calculateMean(bottomPixels, 32);
+    // if (avgValue2 < TH1_2) {
+    //   blob_count_bottom = 0;
+    // }
+    // else if (TH2_count_bottom > binary_TH) {
+    //   blob_count_bottom = 2;
+    // }
+    // else {
+    //   blob_count_bottom = 1;
+    if (TH1_count_bottom < 12) {
       blob_count_bottom = 0;
     }
-    else if (TH2_count_2 > binary_TH) {
-      blob_count_bottom = 2;
-    }
+    // else if (TH2_count_bottom > binary_TH) {
+    //   blob_count_bottom = 2;
+    // }
     else {
       blob_count_bottom = 1;
+
+      int maxIndex2;
+      int maxValue2 = findMaxAndIndex(bottomPixels, 32, maxIndex2);
+      Serial.print("Bottom max value at [");
+      Serial.print(maxIndex2 / 8);
+      Serial.print("][");
+      Serial.print(maxIndex2 % 8);
+      Serial.print("] = ");
+      Serial.println(maxValue2);
     }
     Serial.print("Blob count for bottom half: ");
     Serial.println(blob_count_bottom);
   }
 
   Serial.println();
-  delay(1000);
+  delay(100);
 
 }
 
@@ -189,8 +238,32 @@ float calculateStandardDeviation(float arr[], int size, float mean) {
   return sqrt(sumOfSquares / size);  // Standard deviation formula
 }
 
-float applyEquation(float Tsa, float Tamb) {
+float calculateMax(float arr[], int size) {
+  float maxValue = arr[0];
+  for (int i = 1; i < size; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+    }
+  }
+  return maxValue;
+}
+
+float applyEquation(float Tsa) {
   // Apply the equation to convert the temperature
   float Tv = pow(pow(Tsa + 273, 4) + (h * (Tsa - Tamb)) / (sigma * epsilon), 0.25) - 273;
   return Tv;
+}
+
+float findMaxAndIndex(float arr[], int size, int &maxIndex) {
+  float maxValue = arr[0];  // Assume the first element is the maximum initially
+  maxIndex = 0;  // Index of the first element
+
+  for (int i = 1; i < size; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+      maxIndex = i;  // Update the index of the maximum value
+    }
+  }
+
+  return maxValue;
 }
